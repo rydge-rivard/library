212d249d1f16ae0a7eb8236de4c09f24bcbfcbe5
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-10-25 12:55:51 +0100

    Add v1 of dom function to display books in array.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const myLibrary = [];
 
 function Book(title, author, pages, read) {
     this.title = title;
-    this.autor = author;
+    this.author = author;
     this.pages = pages;
     this.read = read;
     this.info(this);
@@ -24,3 +24,22 @@ const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'read');
 const theHobbitTwo = new Book('The Hobbit Two', 'J.R.R. Tolkien', '305', 'not read');
 const theHobbitThree = new Book('The Hobbit Three', 'J.R.R. Tolkien', '340', 'not read');
 
+function newBook() {
+    myLibrary.forEach(book => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        document.body.appendChild(div);
+        let i = 0;
+        for(let key in book){
+            if(key === 'title' || key === 'author' || key === 'pages' || key === 'read') {
+                console.log(book[key]);
+                let category = Object.keys(book)[i];
+                let upperCategory = category.charAt(0).toUpperCase() + category.slice(1);
+                const p = document.createElement('p');
+                p.textContent = `${upperCategory}: ${book[key]}`;
+                div.appendChild(p);
+                i = i + 1;
+            }
+      }
+    });   
+}
